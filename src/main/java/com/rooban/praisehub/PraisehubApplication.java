657d8b10103cac10f5dcdb130772343e95ad377e
Wanleyrooby <alexiswanley25@gmail.com>
2025-11-27 19:43:25 -0300

    Primeiro commit

---
+++ src/main/java/com/rooban/praisehub/PraisehubApplication.java
@@ -0,0 +1,13 @@
+package com.rooban.praisehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraisehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraisehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rooban/praisehub/PraisehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rooban.praisehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraisehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
